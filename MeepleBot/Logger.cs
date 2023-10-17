ba65562fa757d7c1aca92902352e58a4db31339c
Stephen <stephen.jr@selyuk.com>
2023-10-17 18:08:57 +0300

    Initial

---
+++ MeepleBot/Logger.cs
@@ -0,0 +1,26 @@
+﻿using NotEnoughLogs;
+using NotEnoughLogs.Behaviour;
+
+namespace MeepleBot;
+
+public sealed class Logging
+{
+   private static readonly Lazy<Logging> _instance = new(() => new Logging());
+   public static Logging Instance => _instance.Value;
+   public Logger Logger { get; }
+   private Logging()
+   {
+      var configuration = new LoggerConfiguration
+      {
+         Behaviour = new DirectLoggingBehaviour(),
+         MaxLevel = LogLevel.Trace
+      };
+      Logger = new Logger(configuration);
+   }
+}
+
+enum Logs
+{
+   Token,
+   DiscordError
+}
---
+++ MeepleBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus;
+using System.Threading.Tasks;
+namespace MeepleBot;
+
+static class MeepleBot
+{
+    static async Task Main()
+    {
+        DiscordClient client = new(new DiscordConfiguration()
+        {
+            Token = (await GetToken()),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.Guilds,
+        });
+        await client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+
+    static Task<String> GetToken()
+    {
+        var token = String.Empty;
+        try
+        {
+            token = File.ReadAllText("token.txt");
+        }
+        catch (FileNotFoundException ex)
+        {
+            Logging.Instance.Logger.LogCritical(Logs.Token, "token.txt was not found, exiting");
+            Task.Delay(1000);
+            Environment.Exit(1);
+        }
+
+        return Task.Run(() => token);
+    }
+}
