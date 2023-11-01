1640f981f4a11823102de87c3655e330f1cc9b7d
Stephen <stephen.jr@selyuk.com>
2023-11-01 13:42:41 +0200

    Change the Apply command to use RealmDatabaseService.cs

--- MeepleBot/commands/Apply.cs
+++ MeepleBot/commands/Apply.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using MeepleBot.database;
 using MeepleBot.objects;
 using Realms;
 
@@ -19,33 +20,15 @@ public class ApplicationCommand : ApplicationCommandModule
     )
     {
         await context.DeferAsync();
-        DateTimeOffset time = new(DateTime.UtcNow);
-        string unixTime = time.ToUnixTimeMilliseconds().ToString();
-
+        var databaseService = new RealmDatabaseService();
         try
-        {
-            using (var localRealm = await Realm.GetInstanceAsync())
+        { 
+            if (!await databaseService.ApplicationExists(context.User.Id.ToString())) 
             {
-                if (!localRealm.All<Application>().Any(application => application.DiscordId == context.User.Id.ToString()))
-                {
-                    await localRealm.WriteAsync(() =>
-                    {
-                        localRealm.Add(new Application
-                        {
-                            Time = unixTime,
-                            DiscordId = context.User.Id.ToString(),
-                            Game = game,
-                            Username = username,
-                        });
-                    });
-                }
-                else
-                {
-                    await context.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("You have already applied."));
-                    return;
-                }
-                
+                await databaseService.CreateApplication(context.User.Id.ToString(), game, username);
             }
+            else { await context.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("You have already applied.")); return; }
+             
             await context.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("You have successfully applied"));
             Logging.Logger.LogInfo(Logs.Discord, $"{context.User.Username} ran the /apply command. \nParams: {username}, {game}");
         }
--- MeepleBot/database/RealmDatabaseService.cs
+++ MeepleBot/database/RealmDatabaseService.cs
@@ -1,3 +1,5 @@
+using DSharpPlus.Entities;
+using MeepleBot.objects;
 using Realms;
 
 namespace MeepleBot.database;
@@ -14,4 +16,25 @@ public class RealmDatabaseService
         };
         _realm = Realm.GetInstance(config);
     }
+    public async Task CreateApplication(string id, string game, string username)
+    {
+        DateTimeOffset time = new(DateTime.UtcNow);
+        string unixTime = time.ToUnixTimeMilliseconds().ToString();
+        await _realm.WriteAsync(() =>
+        {
+            _realm.Add(new Application
+            {
+                Time = unixTime,
+                DiscordId = id,
+                Game = game,
+                Username = username,
+            });
+        });
+    }
+
+    public Task<bool> ApplicationExists(string id)
+    {
+        return Task.FromResult(_realm.All<Application>().Any(application => application.DiscordId == id));
+    }
+    
 }
